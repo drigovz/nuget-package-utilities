c3c8953e1fc4b322b30c409bdc00f4fe9dfec9c6
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-12-26 16:54:02 -0300

    test: Add github actions

--- src/Utils.Extensions/Collections/EnumerableExtension.cs
+++ src/Utils.Extensions/Collections/EnumerableExtension.cs
@@ -4,7 +4,13 @@ namespace System.Collections.Generic
 {
     public static class EnumerableExtension
     {
-        public static bool IsNullOrEmpty<T>(this IEnumerable<T> source)
+        /// <summary>
+        /// Verify if collection is null or empty
+        /// </summary>
+        /// <returns>
+        /// return true if is null or empty
+        /// </returns>
+        public static bool IsNullOrEmpty<T>(this List<T> source)
             => source == null || !source.Any();
     }
 }
